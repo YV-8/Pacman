1aa86a212d44d6edb6ec0f0b629e5cd08c8fcfab
Moni <monica.escobar0125@jala.university>
2026-01-28 21:29:52 -0400

    US7-Add method and momdificated structure

--- Pacman/PacmanSolution/ViewModels/GamePageViewModel.cs
+++ Pacman/PacmanSolution/ViewModels/GamePageViewModel.cs
@@ -1,23 +1,20 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PacmanSolution.Models;
 
 namespace PacmanSolution.ViewModels;
 
-public class GamePageViewModel: INotifyPropertyChanged
+public partial class GamePageViewModel: ViewModelBase
 {
-    public event PropertyChangedEventHandler? PropertyChanged;
+    [ObservableProperty]
+    private Board _gameBoard;
 
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    public GamePageViewModel()
     {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(propertyName);
-        return true;
+        // Inicializamos el tablero (ej. 10x10)
+        GameBoard = new Board(10, 10);
+        GameBoard.CreateBoard();
     }
 }
--- Pacman/PacmanSolution/ViewModels/MainWindowViewModel.cs
+++ Pacman/PacmanSolution/ViewModels/MainWindowViewModel.cs
@@ -1,43 +1,18 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace PacmanSolution.ViewModels;
 
-public partial class MainWindowViewModel:INotifyPropertyChanged
+public partial class MainWindowViewModel:ViewModelBase
 {
+    [ObservableProperty]
     private object _currentPage;
-
-    public object CurrentPage
-    {
-        get => _currentPage;
-        set
-        {
-            if (_currentPage != value)
-            {
-                _currentPage = value;
-                OnPropertyChanged();
-            }
-        }
-    }
 
     public MainWindowViewModel()
     {
         CurrentPage = new MainPageViewM(this);
     }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(propertyName);
-        return true;
-    }
+    
 }
