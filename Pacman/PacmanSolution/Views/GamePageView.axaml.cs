c67b12667c39af0fc878fd4b0f80c5d0fc1d1523
Moni <monica.escobar0125@jala.university>
2026-02-04 22:16:05 -0400

    Modificate the VIews

--- Pacman/PacmanSolution/Views/GamePageView.axaml.cs
+++ Pacman/PacmanSolution/Views/GamePageView.axaml.cs
@@ -1,6 +1,5 @@
 using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using Avalonia;
+using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Input;
@@ -8,19 +7,21 @@ using Avalonia.Interactivity;
 using PacmanSolution.Models;
 using PacmanSolution.ViewModels;
 using Avalonia.Media;
-using Avalonia.Threading;
 
 namespace PacmanSolution.Views;
 
 public partial class GamePageView : UserControl
 {
     private ObservableCollection<Cell> _cells;
+    
+    
     public GamePageView()
     {
         InitializeComponent();
         DataContext = new  GamePageViewModel();
         KeyDown += OnKeyDown;
         this.Loaded += OnLoaded;
+        OnPelletEaten();
     }
 
     private void OnLoaded(object? sender, RoutedEventArgs e)
@@ -42,29 +43,58 @@ public partial class GamePageView : UserControl
     public void DrawBoard(ObservableCollection<Cell> board)
     {
         if (GameCanvas == null || board == null) return;
-        GameCanvas.Children.Clear();
-
+        double cellSize = 40; 
+    
+        // Márgenes para centrar la rejilla sobre el dibujo del Board.png
+        double offsetX = 60; 
+        double offsetY = 15;
+        //GameCanvas.Children.Clear();
+        var elementsToRemove = GameCanvas.Children
+            .Where(x => x != PacmanImage && !(x is Image))
+            .ToList();
+        foreach (var child in elementsToRemove)
+        {
+            GameCanvas.Children.Remove(child);
+        }
         foreach (var cell in board)
         {
-            if (cell.Type == CellType.Wall)
+            if (cell.Type == CellType.WALL || cell.Type == CellType.INSIDEWALL)
             {
-                var wall = new Rectangle { Width = 40, Height = 40, Fill = Brushes.Blue };
-                Canvas.SetLeft(wall, cell.Column * 40);
-                Canvas.SetTop(wall, cell.Row * 40);
+                var wall = new Rectangle { Width = 20, Height = 20, Fill = Brushes.Blue };
+                wall.ZIndex = 2;
+                Canvas.SetLeft(wall, offsetX + (cell.Column * cellSize));
+                Canvas.SetTop(wall, offsetY + (cell.Row * cellSize));
                 GameCanvas.Children.Add(wall);
             }
-            
+            if (cell.Type == CellType.Energize)
+            {
+                var powerPellet = new Ellipse { Width = 35, Height = 35, Fill = Brushes.White };
+                ZIndex = 2;
+                double xPos = offsetX + (cell.Column * cellSize) + (cellSize - 16) / 2;
+                double yPos = offsetY + (cell.Row * cellSize) + (cellSize - 16) / 2;
+                Canvas.SetLeft(powerPellet, xPos); 
+                Canvas.SetTop(powerPellet, yPos);
+                GameCanvas.Children.Add(powerPellet);
+            }
             if (cell.HasPellet)
             {
-                var dot = new Ellipse { Width = 8, Height = 8, Fill = Brushes.Yellow };
-                // Centramos el punto en el cuadro de 40x40
-                Canvas.SetLeft(dot, (cell.Column * 40) + 16); 
-                Canvas.SetTop(dot, (cell.Row * 40) + 16);
+                var dot = new Rectangle { Width = 12, Height = 12, Fill = Brushes.CadetBlue };
+                ZIndex = 3;
+                double xPos = offsetX + (cell.Column * cellSize) + (cellSize -40) / 5;
+                double yPos = offsetY + (cell.Row * cellSize) + (cellSize - 18) / 2;
+                Canvas.SetLeft(dot, xPos); 
+                Canvas.SetTop(dot, yPos);
                 GameCanvas.Children.Add(dot);
             }
         }
     }
-
+    private void OnPelletEaten()
+    {
+        if (DataContext is GamePageViewModel vm)
+        {
+            vm.Score += 10;
+        }
+    }
     private void ManagePacman()
     {
         
