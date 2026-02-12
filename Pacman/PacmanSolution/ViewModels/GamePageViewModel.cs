7581233a06c71779fce59a582dadb2a78c3b50a2
Moni <monica.escobar0125@jala.university>
2026-02-12 15:31:42 -0400

    Integrate basic structure method

--- Pacman/PacmanSolution/ViewModels/GamePageViewModel.cs
+++ Pacman/PacmanSolution/ViewModels/GamePageViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using Avalonia.Controls;
@@ -12,20 +13,15 @@ namespace PacmanSolution.ViewModels;
 public partial class GamePageViewModel: ObservableObject
 {
     [ObservableProperty]
-    private int _score;
-    [ObservableProperty]
-    private int _highScore;
-    [ObservableProperty]
     private ManagePageChange _navigation;
     [ObservableProperty]
     private IImage? _pacmanCurrentSprite;
     private EngineManager _engineManager;
-    private ObservableCollection<Entity> _board = new();
     private SoundManager _soundManager = new ();
     private SpriteManager _spriteManager = new ();
     private DispatcherTimer _gameTimer;
-    private int _animationFrame = 0;
-
+    private DispatcherTimer? _gameLoopTimer;
+    private ObservableCollection<Entity> _board = new();
     public ObservableCollection<Entity> Board
     {
         get => _board;
@@ -34,14 +30,20 @@ public partial class GamePageViewModel: ObservableObject
 
     public GamePageViewModel(ManagePageChange navigation)
     {
+        /*InitializePacmanPosition();
+        _gameLoopTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(150) };
+        _gameLoopTimer.Tick += (s, e) => {
+            MovePacman();
+        };*/
         _navigation = navigation;
         Board.Clear();
         _engineManager = new EngineManager(28,31);//31*32
         Score = 0;
         HighScore = 0;
-        
         _engineManager.BuildGameBoard(Board);
+        InitializePacmanPosition();
         StartGameLoop();
+        StartMovementTimer();
     }
     
     [RelayCommand]
@@ -61,5 +63,29 @@ public partial class GamePageViewModel: ObservableObject
         {
             _soundManager.StopSound();
         }
+    }
+    /// <summary>
+    /// Detiene y limpia los timers
+    /// </summary>
+    public void CleanupTimers()
+    {
+        _animationTimer?.Stop();
+        _movementTimer?.Stop();
+        _gameTimer?.Stop();
+    }
+    
+    public void ResumeGame()
+    {
+        _gameTimer?.Start();
+        _movementTimer?.Start();
+    }
+    
+    /// <summary>
+    /// Pause the game
+    /// </summary>
+    public void PauseGame()
+    {
+        _gameTimer?.Stop();
+        _movementTimer?.Stop();
     }
 }
